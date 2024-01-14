3d47480a89810aeff59f4c481f9c57b5bb5e650b
joão vitor <shepherdcom12@gmail.com>
2024-01-14 02:19:22 -0300

    add coments to challenges

--- src/pages/challenge/challenge.js
+++ src/pages/challenge/challenge.js
@@ -1,4 +1,5 @@
-import { actualUserData, thisUserData } from "../../scripts/returnUserInfos"
+import { getComments, postComment, getThisComment } from "../../scripts/postGetcoments"
+import { actualUserData, thisUserData, actualUserEmail } from "../../scripts/returnUserInfos"
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 import { getFirestore, doc, setDoc, onSnapshot, addDoc, collection, query, where, getDocs, serverTimestamp, deleteDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
@@ -85,17 +86,94 @@ async function loadChallenges() {
 }
 
 function ChallengeWindowData(obj, id, url) {
-    challengeWindow.children[1].textContent = `${obj.challengeTitle}`
-    challengeWindow.children[2].children[0].src = `${url}`
-    challengeWindow.children[3].textContent = `${obj.challengeDescription}`
-    challengeWindow.children[5].textContent = `Detalhes da atividade: ${obj.challengeDetails}`
-    challengeWindow.children[7].textContent = `+${obj.challengePoints} Pontos`
-    challengeSection.style.display = "none"
-    challengeWindow.style.display = "flex"
+    actualUserEmail().then(actualUser => {
+        challengeWindow.children[1].textContent = `${obj.challengeTitle}`
+        challengeWindow.children[2].children[0].src = `${url}`
+        challengeWindow.children[3].textContent = `${obj.challengeDescription}`
+        challengeWindow.children[5].textContent = `Detalhes da atividade: ${obj.challengeDetails}`
+        challengeWindow.children[7].textContent = `+${obj.challengePoints} Pontos`
+        loadComents(challengeWindow.children[10].children[0], obj, id, url)
+        challengeWindow.children[10].children[1].children[0].children[2].onclick = function () {
+            if (challengeWindow.children[10].children[1].children[0].children[1].value.replace(" ", "") != "") {
+                postComment("challenges", id, actualUser, challengeWindow.children[10].children[1].children[0].children[1].value).then(posted => {
+                    challengeWindow.children[10].children[1].children[0].children[1].value = ""
+                    unrefreshLoadComents(challengeWindow.children[10].children[0], obj, id, posted, url)
+                })
+            }
+        }
+        challengeSection.style.display = "none"
+        challengeWindow.style.display = "flex"
+    })
 }
 
-thisUserData("gabrielbastos@maker.com").then( UserData => {
-    console.log(UserData);
-})
+function loadComents(section, obj, id, url) {
+    let photoUrl = ""
+    actualUserEmail().then(actualUser => {
+        section.innerHTML = ""
+        getComments("challenges", `${id}`).then(coment => {
+            coment.forEach(element => {
+                thisUserData(element.email).then(UserData => {
+                    if (UserData.noPhoto == true) {
+                        photoUrl = "https://img.freepik.com/vetores-gratis/ilustracao-do-icone-da-lampada_53876-43730.jpg?w=740&t=st=1705192551~exp=1705193151~hmac=3347369c888609a6def2a1cd13bfb02dc519c8fbc965419dd1b5f091ef79982d"
+                    } else {
+
+                    }
+                    let article = document.createElement("article")
+                    section.insertAdjacentElement("beforeend", article)
+                    article.classList.add(`${element.email == actualUser ? "actualUser" : "otherUser"}`)
+                    article.classList.add(`commentCard`)
+                    article.innerHTML = `
+                <div class="commentCard__div--1">
+                    <div class="commentCard__resetPhoto">
+                        <img class="commentCard__photo" src="${photoUrl}">
+                    </div>
+                    <p class="commentCard__dateTime">${element.date}<br>${element.time}</p>
+                </div>
+                <div class="commentCard__div--2">
+                    <p class="commentCard__name">${element.email == actualUser ? "" : `<span class="commentCard__clasRoom">${UserData.class}${UserData.room}</span>`}${UserData.name}</p>
+                    <div class="commentCard__resetText">
+                        <p class="commentCard__text">${element.text}</p>
+                    </div>
+                </div>`
+                    article.style.order = `${element.timestamp.seconds}`
+
+                });
+            })
+        })
+    })
+}
+
+function unrefreshLoadComents(section, obj, id, postId, url) {
+    let photoUrl = ""
+    actualUserEmail().then(actualUser => {
+        getThisComment("challenges", id, postId).then(element => {
+            thisUserData(element.email).then(UserData => {
+                if (UserData.noPhoto == true) {
+                    photoUrl = "https://img.freepik.com/vetores-gratis/ilustracao-do-icone-da-lampada_53876-43730.jpg?w=740&t=st=1705192551~exp=1705193151~hmac=3347369c888609a6def2a1cd13bfb02dc519c8fbc965419dd1b5f091ef79982d"
+                } else {
+
+                }
+                let article = document.createElement("article")
+                section.insertAdjacentElement("beforeend", article)
+                article.classList.add(`${element.email == actualUser ? "actualUser" : "otherUser"}`)
+                article.classList.add(`commentCard`)
+                article.innerHTML = `
+                    <div class="commentCard__div--1">
+                        <div class="commentCard__resetPhoto">
+                            <img class="commentCard__photo" src="${photoUrl}">
+                        </div>
+                        <p class="commentCard__dateTime">${element.date}<br>${element.time}</p>
+                    </div>
+                    <div class="commentCard__div--2">
+                        <p class="commentCard__name">${element.email == actualUser ? "" : `<span class="commentCard__clasRoom">${UserData.class}${UserData.room}</span>`}${UserData.name}</p>
+                        <div class="commentCard__resetText">
+                            <p class="commentCard__text">${element.text}</p>
+                        </div>
+                    </div>`
+                article.style.order = `${element.timestamp.seconds}`
+            });
+        })
+    })
+}
 
 loadChallenges()
---
+++ src/scripts/postGetcoments.js
@@ -0,0 +1,79 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore, doc, setDoc, onSnapshot, addDoc, collection, getDoc, query, where, getDocs, serverTimestamp, deleteDoc } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getStorage, ref, uploadString, getDownloadURL, deleteObject } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+const firebaseConfig = {
+    apiKey: `${import.meta.env.VITE_API_KEY}`,
+    authDomain: `${import.meta.env.VITE_AUTH_DOMAIN}`,
+    projectId: `${import.meta.env.VITE_PROJECT_ID}`,
+    storageBucket: `${import.meta.env.VITE_STORAGE_BUCKET}`,
+    messagingSenderId: `${import.meta.env.VITE_MESSAGING_SENDER_ID}`,
+    appId: `${import.meta.env.VITE_APP_ID}`,
+};
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+function actualDate() {
+    var data = new Date();
+    var dia = resetDate(data.getDate());
+    var mes = resetDate(data.getMonth() + 1);
+    var ano = resetDate(data.getFullYear() % 100);
+    return dia + '/' + mes + '/' + ano;
+}
+function resetDate(numero) {
+    return numero < 10 ? '0' + numero : numero;
+}
+function actualTime() {
+    var data = new Date();
+    var horas = resetTime(data.getHours());
+    var minutos = resetTime(data.getMinutes());
+    return horas + ':' + minutos;
+}
+function resetTime(numero) {
+    return numero < 10 ? '0' + numero : numero;
+}
+
+var horaAtualFormatada = actualTime();
+let dataAtualFormatada = actualDate();
+
+
+
+
+
+export async function postComment(databaseName, id, email, text) {
+    return new Promise(async resolve => {
+        const docRef = await addDoc(collection(db, `${databaseName}`, `${id}`, "coments"), {
+            timestamp: serverTimestamp(),
+            date: `${dataAtualFormatada}`,
+            time: `${horaAtualFormatada}`,
+            email: `${email}`,
+            text: `${text}`
+        });
+        resolve(docRef.id);
+    })
+}
+
+export async function getComments(databaseName, id) {
+    return new Promise(async resolve => {
+        let allcomments = []
+        const querySnapshot = await getDocs(collection(db, `${databaseName}`, `${id}`, "coments"));
+        querySnapshot.forEach((doc) => {
+            allcomments.push(doc.data())
+        });
+        resolve(allcomments);
+    })
+}
+
+export async function getThisComment(databaseName, id, comentId) {
+    return new Promise(async resolve => {
+        const docRef = doc(db, `${databaseName}`, `${id}`, "coments", `${comentId}`);
+        const docSnap = await getDoc(docRef);
+        if (docSnap.exists()) {
+            resolve(docSnap.data())
+        } else {            
+            resolve("No such document!");
+        }
+    })
+}
